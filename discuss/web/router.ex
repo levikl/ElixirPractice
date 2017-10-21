24ddfdcc6d15c984354164197f217b0082b7bdf3
levikl <klingler.59@osu.edu>
2017-10-21 10:42:51 -0400

    Sign-out function, sign-out and sign-in link to template, clear session if sign-out

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -7,6 +7,7 @@ defmodule Discuss.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Discuss.Plugs.SetUser
   end
 
   pipeline :api do
@@ -27,8 +28,11 @@ defmodule Discuss.Router do
   scope "/auth", Discuss do
     pipe_through :browser
 
+    get "/signout", AuthController, :signout
     get "/:provider", AuthController, :request
     get "/:provider/callback", AuthController, :callback
+    # did not have to implement request function; comes out-of-the-box (with ueberauth?)
+    # however, it _was_ necessary to implement a callback function
   end
 
   # Other scopes may use custom stacks.
